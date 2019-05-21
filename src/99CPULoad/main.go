5cb80d3e5d3097c9feb1795721e15075bb742e22
Adam Krieger <akrieger@obsglobal.com>
2019-05-21 16:16:24 -0500

    added load func

---
+++ src/99CPULoad/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "log"
+
+//https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/mon-scripts.html#mon-scripts-perl_prereq
+
+func main() {
+	var i uint32
+	var def uint32
+	for i = 0; i < (def - 1); i++ {
+		//log.Println(i)
+	}
+
+	log.Println("done")
+}
