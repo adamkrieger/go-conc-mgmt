ecb40c58e98a8e5a5a4245b83ae3f9aebcb8a9bc
adamkrieger <adamkrieger.ca@gmail.com>
2019-06-04 15:11:37 -0500

    refined example

--- src/40PanicRecover/textbook/main.go
+++ src/40PanicRecover/textbook/main.go
@@ -17,9 +17,7 @@ func process() {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("recovered into the defer in process()", r)
-			// for a, b := range r {
-			// 	fmt.Println("each: ", a, " ", b)
-			// }
+
 			if _, is := r.(*specificError); is {
 				fmt.Println("-is of type error")
 			} else {
@@ -29,22 +27,18 @@ func process() {
 	}()
 
 	fmt.Println("processing")
-	g(0, "thing")
+	usefulFunction(0, "thing")
 	fmt.Println("returned normally")
 }
 
-func g(i int, some string) {
+func usefulFunction(i int, some string) {
 	if i > 3 {
 		fmt.Println("panicking!")
-		//panic(fmt.Sprintf("%v %s", i, some))
-		//arr := []string{some}
 
 		panic(&specificError{code: 404, msg: "not found"})
-
-		//fmt.Println(arr[100])
 	}
 
 	defer fmt.Println("defer in g", i, some)
 	fmt.Println("printing in g", i, some)
-	g(i+1, some)
+	usefulFunction(i+1, some)
 }
